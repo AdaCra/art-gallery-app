3408e9a2c2d21fee5b3e4fd355c1f99a79d696c2
Adam Hannath <ahannath@gmail.com>
2023-04-26 16:00:09 +0200

    setup

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Gallery-App</h1>
+      <p>
+        nav <br />
+        component
+        <br /> footer
+      </p>
     </div>
   );
 }
