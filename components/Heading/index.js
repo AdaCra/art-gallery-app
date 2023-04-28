8e60608f024ab6376f272dec5ab181bd30096714
Denes <csaszar.denes@gmail.com>
2023-04-28 13:24:34 +0200

    header-styled-component

--- components/Heading/index.js
+++ components/Heading/index.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import styled from "styled-components";
 
 export default function Heading() {
   return (
@@ -12,3 +13,19 @@ export default function Heading() {
     </header>
   );
 }
+
+const header = styled.div`
+   {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 2rem;
+    padding: 1rem;
+    background-color: #f5f5f5;
+    border-radius: 0.5rem;
+    box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.25);
+    text-align: center;
+    color: #333;
+    font-family: "Roboto", sans-serif;
+  }
+`;
